c8920252214355b09803a84fc5e904f741717090
Mostafijur Rahman <mostafijurrmitun@gmail.com>
2019-10-02 01:22:08 +0600

    work on expandable list view

--- app/src/main/java/com/fendonus/retrofit/fragment/VideosFragment.java
+++ app/src/main/java/com/fendonus/retrofit/fragment/VideosFragment.java
@@ -11,11 +11,14 @@ import android.view.ViewGroup;
 
 import com.fendonus.retrofit.R;
 
+import java.util.List;
+
 /**
  * A simple {@link Fragment} subclass.
  */
 public class VideosFragment extends Fragment {
 
+    private List<String> listDataGroup;
 
     public VideosFragment() {
         // Required empty public constructor
@@ -26,7 +29,13 @@ public class VideosFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_videos, container, false);
+        View view = inflater.inflate(R.layout.fragment_videos, container, false);
+
+        listDataGroup.add(getString(R.string.text_accounts));
+        listDataGroup.add(getString(R.string.text_uses_smart_phone_about));
+        listDataGroup.add(getString(R.string.text_uses_computer_about));
+        listDataGroup.add(getString(R.string.text_basic_mind_qna));
+        return view;
     }
 
 }
